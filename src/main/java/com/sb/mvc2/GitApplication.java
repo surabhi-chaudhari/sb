bee72040931bb834f8bae65395d624e850ead017
surabhi <surabhichaudhari96@gmail.com>
2020-11-19 22:32:55 +0530

    a

---
+++ src/main/java/com/sb/mvc2/GitApplication.java
@@ -0,0 +1,13 @@
+package com.sb.mvc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/mvc2/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.mvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
